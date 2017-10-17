841c3f1dcfc8e46573242fd86e814ed335d8eb02
Reena Paranjape <reena.paranjape@here.com>
2017-10-16 21:02:07 -0400

    Script to connect front end search bar to elasticsearch and display pulled up results.

---
+++ elasticsearch-angular1.5/js/script.js
@@ -0,0 +1,119 @@
+
+/**
+ * Create the module. Set it up to use html5 mode.
+ */
+window.myMovies = angular.module('myMovies', ['elasticsearch'],
+    ['$locationProvider', function($locationProvider){
+        $locationProvider.html5Mode(true);
+    }]
+);
+
+/**
+ * Create a service to power calls to Elasticsearch. We only need to
+ * use the _search endpoint.
+ */
+myMovies.factory('movieService',
+    ['$q', 'esFactory', '$location', function($q, elasticsearch, $location){
+        var client = elasticsearch({
+            host: $location.host() + ":9200"
+        });
+
+        /**
+         * Given a term and an offset, load another round of 10 movies.
+         *
+         * Returns a promise.
+         */
+        var search = function(term, offset){
+            var deferred = $q.defer();
+            var query = {
+                "match": {
+                    "_all": term
+                }
+            };
+
+            client.search({
+                "index": 'testmovies',
+                "type": 'movie',
+                "body": {
+                    "size": 10,
+                    "from": (offset || 0) * 10,
+                    "query": query
+                }
+            }).then(function(result) {
+                var ii = 0, hits_in, hits_out = [];
+                hits_in = (result.hits || {}).hits || [];
+                for(;ii < hits_in.length; ii++){
+                    hits_out.push(hits_in[ii]._source);
+                }
+                deferred.resolve(hits_out);
+            }, deferred.reject);
+
+            return deferred.promise;
+        };
+
+
+        return {
+            "search": search
+        };
+    }]
+);
+
+/**
+ * Create a controller to interact with the UI.
+ */
+myMovies.controller('movieCtrl',
+    ['movieService', '$scope', '$location', function(movies, $scope, $location){
+        // Provide some nice initial choices
+        var initChoices = [
+            "Arrival",
+            "Gravity",
+            "Avatar",
+            "Up",
+            "Schindlers List",
+            "Despicable Me",
+            "It",
+        ];
+        var idx = Math.floor(Math.random() * initChoices.length);
+
+        // Initialize the scope defaults.
+        $scope.movies = [];        // An array of movie results to display
+        $scope.page = 0;            // A counter to keep track of our current page
+        $scope.allResults = false;  // Whether or not all results have been found.
+
+        // And, a random search term to start if none was present on page load.
+        $scope.searchTerm = $location.search().q || initChoices[idx];
+
+        /**
+         * A fresh search. Reset the scope variables to their defaults, set
+         * the q query parameter, and load more results.
+         */
+        $scope.search = function(){
+            $scope.page = 0;
+            $scope.movies = [];
+            $scope.allResults = false;
+            $location.search({'q': $scope.searchTerm});
+            $scope.loadMore();
+        };
+
+        /**
+         * Load the next page of results, incrementing the page counter.
+         * When query is finished, push results onto $scope.movies and decide
+         * whether all results have been returned (i.e. were 10 results returned?)
+         */
+        $scope.loadMore = function(){
+            movies.search($scope.searchTerm, $scope.page++).then(function(results){
+                if(results.length !== 10){
+                    $scope.allResults = true;
+                }
+
+                var ii = 0;
+                for(;ii < results.length; ii++){
+                    $scope.movies.push(results[ii]);
+                }
+            });
+        };
+
+        // Load results on first run
+        $scope.loadMore();
+    }]
+);
