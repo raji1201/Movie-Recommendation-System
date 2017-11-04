0e311935080bcbafa73f084651d98377480772f5
raji1201 <rajis.id@gmail.com>
2017-11-04 19:51:58 -0400

    Sign up changes

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Http, Response, RequestOptions, Headers} from '@angular/http';
@@ -12,14 +12,23 @@ import { User } from './user';
 })
 
 export class SignUpComponent {
-	constructor(private http: Http) {}
+	constructor(private http: Http, private router: Router) {}
 	submitted = false;
 	model = new User ('', '', '', '');
 
 	onSubmit(form: NgForm) {
-		let formData = form.value;
-		console.log(formData);
-		const req = this.http.post('/signupuser', formData);
-		req.subscribe();	
-	}
+    let formData = form.value;
+    //console.log(formData);
+    const req = this.http.post('/signupuser', formData);
+    req.subscribe(
+    	res => {
+          var response = res["_body"];
+          //console.log(JSON.parse(response)['name']);
+          var name = JSON.parse(response)['name'];
+          this.router.navigate(['/profile', name]);
+        },
+        err => {
+          console.log("ERROR");
+        });  
+  }
 }
