53d4038983e3d91d5287461b43af196384883e8d
raji1201 <rajis.id@gmail.com>
2017-11-04 19:45:19 -0400

    Included imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   	const req = this.http.post('/home', null);
