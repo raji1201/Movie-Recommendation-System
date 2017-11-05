bbee40f041efb259b9f0033dac35b3d923634401
Reena Paranjape <reena.paranjape@here.com>
2017-11-04 23:07:25 -0700

    Implement method onSubmit() for Search button on home page

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 import { Router, ActivatedRoute, Params, RoutesRecognized } from '@angular/router';
+import {ElasticsearchService} from '../elasticsearch.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +10,44 @@ import { Router, ActivatedRoute, Params, RoutesRecognized } from '@angular/route
 })
 export class NavComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  isConnected = false;
+
+  form: FormGroup;
+  status: string;
+
+  constructor(private route: ActivatedRoute, private router: Router, private fbuilder: FormBuilder, private es: ElasticsearchService, private cd: ChangeDetectorRef ) {
+    this.isConnected = false;
+
+    this.form = fbuilder.group({
+      index: '',
+    });
+  }
 
   ngOnInit() {
     this.isUserLoggedIn();
+    this.es.isAvailable().then(() => {
+      this.status = 'OK';
+      this.isConnected = true;
+    }, error => {
+      this.status = 'ERROR';
+      this.isConnected = false;
+      console.error('Server is down', error);
+    }).then(() => {
+      this.cd.detectChanges();
+    });
+  }
+
+  onSubmit(value) {
+
+    this.es.createIndex({ index: value.index}).then(
+      result => {
+        console.log(result);
+        alert('Index added, see log for more info');
+      }, error => {
+        alert('Something went wrong, see log for more info');
+        console.error(error);
+      }
+    );
   }
 
   isUserLoggedIn()
