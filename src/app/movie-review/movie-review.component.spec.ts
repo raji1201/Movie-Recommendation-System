8baff4e671887bc260e7d45a2e7261a63cbe6a7a
Manoj <manojp.1902@gmail.com>
2017-11-05 22:35:49 -0500

    Test

--- src/app/movie-review/movie-review.component.spec.ts
+++ src/app/movie-review/movie-review.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MovieReviewComponent } from './movie-review.component';
 import { MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
-
+//test
 describe('MovieReviewComponent', () => {
   let component: MovieReviewComponent;
   let fixture: ComponentFixture<MovieReviewComponent>;
